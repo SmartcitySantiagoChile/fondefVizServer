df96d0856340ba90df8ae588fc3b466ba4bfa58a
Felipe Hernández <cephei.1313@gmail.com>
2017-05-02 18:02:54 -0300

    code clean and example of load profile

--- profile/views.py
+++ profile/views.py
@@ -3,6 +3,8 @@ from django.views.generic import View
 from django.http import JsonResponse
 from django.db import connection
 
+from elasticsearch import Elasticsearch
+from elasticsearch_dsl import Search, Q
 # Create your views here.
 
 class LoadProfileByExpeditionView(View):
@@ -27,8 +29,62 @@ class GetLoadProfileByExpeditionData(View):
 
     def get(self, request):
         ''' expedition data '''
+        expeditionId = request.GET.get('idExpedition','')
+
+        client = Elasticsearch("172.17.57.47:9200")
+        s = Search(using=client, index="profiles")\
+            .query('match', idExpedicion=expeditionId)\
+            .query('match', Cumplimiento='C')\
+            .source(['Capacidad', 'Tiempo', 'Patente', 'ServicioSentido', 'Carga', 'idExpedicion', 'NombreParada', 'BajadasExpandidas', 'SubidasExpandidas', 'Correlativo', 'DistEnRuta', 'Hini', 'Hfin'])\
+            .sort('idExpedicion', 'Correlativo')
+        #.query('match', idExpedicion=64000)
+ 
         response = {}
-        # use elasticSearch
-        
+        # debug
+        #response['query'] = s.to_dict()
+        #response['state'] = {'success': answer.success(), 'took': answer.took, 'total': answer.hits.total}
+        response['trips'] = {}
+        for hit in s.scan():
+            data = hit.to_dict()
+
+            expeditionId = data['idExpedicion']
+            if expeditionId not in response['trips']:
+                response['trips'][expeditionId] = {'info': {}, 'stops': []}
+
+            response['trips'][expeditionId]['info']['capacity'] = int(data['Capacidad'])
+            response['trips'][expeditionId]['info']['licensePlate'] = data['Patente']
+            response['trips'][expeditionId]['info']['route'] = data['ServicioSentido']
+            response['trips'][expeditionId]['info']['timeTripInit'] = data['Hini']
+            response['trips'][expeditionId]['info']['timeTripEnd'] = data['Hfin']
+            stop = {}
+            stop['name'] = data['NombreParada']
+            stop['distOnRoute'] = data['DistEnRuta']
+            stop['time'] = data['Tiempo']
+            stop['order'] = int(data['Correlativo'])
+            stop['loadProfile'] = float(data['Carga'])
+            stop['expandedGetIn'] = float(data['BajadasExpandidas'])
+            stop['expandedGetOut'] = float(data['SubidasExpandidas'])
+            response['trips'][expeditionId]['stops'].append(stop)
+
+        for expeditionId in response['trips']:
+            response['trips'][expeditionId]['stops'] = sorted(response['trips'][expeditionId]['stops'], 
+                 key=lambda record: record['order'])
 
         return JsonResponse(response, safe=False)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
