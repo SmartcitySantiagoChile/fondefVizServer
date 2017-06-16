e257b11ec325f5659d6f9b4fca9fb3003534ed22
Matías Pavez <matias.pavez.b@gmail.com>
2017-06-16 14:53:06 -0400

    indicators template

--- indicators/urls.py
+++ indicators/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
 from .views import common
 
-app_name='indicators'
+app_name = 'indicators'
 urlpatterns = [
-  url(r'^$', common.index, name='index'),
+  url(r'^time$', common.time, name='time'),
 ]
--- indicators/views/common.py
+++ indicators/views/common.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 
 
-def index(request):
-    return render(request, "indicators/index.html", {})
+def time(request):
+    return render(request, "indicators/byTravelTime.html", {})
