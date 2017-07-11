e99997403b922cb7a318af823b52645e743fe009
Matías Pavez <matias.pavez.b@gmail.com>
2017-07-11 18:28:08 -0400

    add HalfHour and Commune models, migrations and fixtures.

--- localinfo/models.py
+++ localinfo/models.py
@@ -21,3 +21,29 @@ class TimePeriod(models.Model):
 
     # End time for the period
     endTime = models.TimeField(auto_now=False, auto_now_add=False)
+
+
+class Commune(models.Model):
+    """ Standard commune data """
+
+    # Id which is used on Elastic Search database
+    esId = models.IntegerField()
+
+    # Commune name on adatrap
+    name = models.CharField(max_length=50)
+
+    # Commune name, pretty version
+    prettyName = models.CharField(max_length=50)
+
+
+class HalfHour(models.Model):
+    """ Represents day time ranges by half hours """
+
+    # Id which is used on Elastic Search database
+    esId = models.IntegerField()
+
+    # Full half hour representation: HH:MM:00
+    name = models.CharField(max_length=10)
+
+    # Abbr half hour representation: HH:MM
+    shortName = models.CharField(max_length=10)
