60206510d588d34edffe402f8f870996de48de4a
Felipe Hernández <cephei.1313@gmail.com>
2017-07-11 19:31:32 -0400

    moved js files to bowerapp because it is its responsability

--- fondefVizServer/settings.py
+++ fondefVizServer/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/fondefviz/static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Elastic-Search settings
