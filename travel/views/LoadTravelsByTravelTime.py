d48fe09a40cfc79da2f8d27809e27638d58770b0
Matías Pavez <matias.pavez.b@gmail.com>
2017-08-29 12:58:44 -0300

    travel: map ES query redesign

--- travel/views/LoadTravelsByTravelTime.py
+++ travel/views/LoadTravelsByTravelTime.py
@@ -6,7 +6,7 @@ from django.views.generic import View
 from django.http import JsonResponse
 from django.conf import settings
 
-from elasticsearch_dsl import Search, MultiSearch, A
+from elasticsearch_dsl import Search, MultiSearch, A, query
 from errors import ESQueryParametersDoesNotExist, ESQueryDateRangeParametersDoesNotExist, ESQueryResultEmpty
 from LoadTravelsGeneric import LoadTravelsGeneric
 
@@ -58,8 +58,8 @@ class GetLoadTravelsByTravelTimeData(View):
         es_query_dict = dict()
         try:
             es_query_dict['histogram'] = self.build_histogram_query(request)
+            es_query_dict['map'] = self.build_map_query(request)
             # es_query_dict['table'] = self.build_table_query(request)
-            #es_query_dict['map'] = self.build_map_query(request)
         except (ESQueryDateRangeParametersDoesNotExist, ESQueryParametersDoesNotExist, ESQueryResultEmpty) as e:
             response['status'] = e.getStatusResponse()
 
@@ -152,8 +152,6 @@ class GetLoadTravelsByTravelTimeData(View):
         if periods:
             es_query = es_query.filter('terms', periodo_subida=periods)
 
-        print es_query.to_dict()
-
         return es_query
 
     def build_histogram_query(self, request):
@@ -195,17 +193,27 @@ class GetLoadTravelsByTravelTimeData(View):
         es_query = self.build_base_query(request)
 
         # travel time map
-        by_sector_agg = A(
-            'filters',
-            other_bucket_key='sin_sector',
-            # filters={
-            #     's1': ('terms', zona_bajada=["10", "100"])
-            # })
-            )
 
-        es_query.aggs.bucket('por_sectores', by_sector_agg)\
-            .metric('cantidad', 'sum', field='factor_expansion')\
-            .pipeline('acumulado', 'cumulative_sum', buckets_path='cantidad')
+        by_zone_agg = A('terms', field='zona_subida', size=1000)
+
+        zones_las_condes = [287, 288, 289, 290, 291, 292, 293, 294, 295, 296, 297, 298, 299, 300, 301, 302, 303, 304, 305, 306, 307, 308, 309, 310, 311, 312, 313, 314, 315, 316, 317, 318, 319, 320, 321, 322, 323]
+        zones_lo_barnechea = [324, 325, 326, 327, 328, 329, 330, 331, 332, 333, 334, 335, 336]
+        zones_providencia = [494, 495, 496, 497, 498, 499, 500, 501, 502, 503, 504, 505, 506, 507, 508, 509, 510, 511, 512, 513, 514, 515, 516]
+        zones_santiago = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52]
+
+        def add_remaining(es_query, sector, zones):
+            es_query.aggs\
+                .bucket(sector, A('filter', query.Q('terms', zona_bajada=zones)))\
+                .bucket('by_zone', by_zone_agg)\
+                .metric('tviaje', 'avg', field='tviaje')\
+                .metric('n_etapas', 'avg', field='n_etapas')\
+                .metric('distancia_ruta', 'avg', field='distancia_ruta')\
+                .metric('distancia_eucl', 'avg', field='distancia_eucl')
+
+        add_remaining(es_query, 'LAS CONDES', zones_las_condes)
+        add_remaining(es_query, 'LO BARNECHEA', zones_lo_barnechea)
+        add_remaining(es_query, 'PROVIDENCIA', zones_providencia)
+        add_remaining(es_query, 'SANTIAGO', zones_santiago)
 
         # # limit fields
         # return es_query.source(self.default_fields)
