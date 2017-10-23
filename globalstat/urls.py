b24f8b660215f652c6c39bd26552b19d79015c9a
Felipe Hernández <cephei.1313@gmail.com>
2017-10-23 11:21:14 -0300

    added detail url

--- globalstat/urls.py
+++ globalstat/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
-from .views import Show, Data
+from .views import Show, Data, Detail
 
 app_name='globalstat'
 urlpatterns = [
   url(r'^show$', Show.as_view(), name='show'),
   url(r'^data$', Data.as_view(), name='data'),
+  url(r'^detail$', Detail.as_view(), name='detail'),
 ]
