d5e9b2eaac0dc0dc60ee57ccb3b8fc087ef12df6
Felipe Hernández <cephei.1313@gmail.com>
2017-10-25 16:02:59 -0300

    added button in templatetags

---
+++ bowerapp/templatetags/button.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django import template
+from django.utils.html import format_html
+
+register = template.Library()
+
+
+@register.simple_tag
+def button(html_id, label, style="success", size=""):
+
+    content = """
+        <button id="{0}" class="btn btn-{2} btn-round btn-{3}">{1}</button>
+        """
+
+    return format_html(content, html_id, label, style, size)
