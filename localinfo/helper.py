62c7750cb52a3b295b0d19d130d960fba7dd291d
Felipe Hernández <cephei.1313@gmail.com>
2018-02-16 13:46:07 -0300

    added helper for local info

---
+++ localinfo/helper.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from localinfo.models import Operator, Commune, DayType, HalfHour, TimePeriod
+
+
+def _list_parser(list):
+    return [{'value': x[0], 'item': x[1]} for x in list]
+
+
+def get_day_type_list_for_select_input():
+    return _list_parser(DayType.objects.values_list('esId', 'name'))
+
+
+def get_operator_list_for_select_input():
+    return _list_parser(Operator.objects.values_list('esId', 'name'))
+
+
+def get_timeperiod_list_for_select_input():
+    return _list_parser(TimePeriod.objects.values_list('esId', 'transantiagoPeriod'))
+
+
+def get_halfhour_list_for_select_input():
+    return _list_parser(HalfHour.objects.values_list('esId', 'name'))
+
+
+def get_commune_list_for_select_input():
+    return _list_parser(Commune.objects.values_list('esId', 'name'))
