76be771a429fb8f34b363d013d40d258249553aa
Felipe Hernández <cephei.1313@gmail.com>
2018-02-20 11:12:32 -0300

    resume data comes from esapi

--- travel/urls.py
+++ travel/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 
-from .views.map import (LoadMapView, GetMapData)
+from .views.map import MapHTML
 from .views.resume import ResumeHTML
 from .views.large import (LoadLargeTravelsView, GetLargeTravelsData)
 from .views.from_to import (LoadFromToMapsView, GetFromToMapsData)
@@ -8,12 +8,11 @@ from .views.from_to import (LoadFromToMapsView, GetFromToMapsData)
 
 app_name = 'travel'
 urlpatterns = [
-  url(r'^$', LoadMapView.as_view(), name='map'),  # dafaults to map
-  url(r'^map$', LoadMapView.as_view(), name='map'),
+  url(r'^$', MapHTML.as_view(), name='map'),  # dafaults to map
+  url(r'^map$', MapHTML.as_view(), name='map'),
   url(r'^resume$', ResumeHTML.as_view(), name='graphs'),
   url(r'^large-travels$', LoadLargeTravelsView.as_view(), name='large-travels'),
   url(r'^fromToMaps$', LoadFromToMapsView.as_view(), name='from-to'),
-  url(r'^getMapData$', GetMapData.as_view()),
   url(r'^getLargeTravelsData$', GetLargeTravelsData.as_view()),
   url(r'^getFromToMapsData$', GetFromToMapsData.as_view())
 ]
