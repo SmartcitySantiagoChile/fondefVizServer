8a7511f637b706d3adc14085de57b1345aa1b423
Felipe Hernández <cephei.1313@gmail.com>
2018-02-20 11:11:50 -0300

    refactoring

--- globalstat/urls.py
+++ globalstat/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import url
 from .views import ResumeHTML, DetailHTML
 
-app_name='globalstat'
+app_name = 'globalstat'
 urlpatterns = [
-  url(r'^resume$', ResumeHTML.as_view(), name='resume'),
-  url(r'^detail$', DetailHTML.as_view(), name='detail'),
+    url(r'^resume$', ResumeHTML.as_view(), name='resume'),
+    url(r'^detail$', DetailHTML.as_view(), name='detail'),
 ]
