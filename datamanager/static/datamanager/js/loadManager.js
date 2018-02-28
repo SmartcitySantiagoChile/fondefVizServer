032073c2a631e79dc6dfa24a6a9cf9c34f62d240
Felipe Hernández <cephei.1313@gmail.com>
2018-02-27 23:11:44 -0300

    refactoring

--- datamanager/static/datamanager/js/loadManager.js
+++ datamanager/static/datamanager/js/loadManager.js
@@ -1,6 +1,6 @@
 "use strict";
 $(document).ready(function () {
-    $.get(Urls["datamanager:getLoadFileData"](), function () {
+    $.get(Urls["datamanager:getLoadFileData"](), function (data) {
         console.log(data);
         var dictFiles = data["routeDictFiles"];
 
