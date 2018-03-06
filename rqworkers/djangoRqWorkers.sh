9e9cd73aa8d751e30ccd2ecb11d52381dbfed596
Felipe Hernández <cephei.1313@gmail.com>
2018-03-06 16:08:33 -0300

    replaced statics path with keywords to be replaced in installation process

--- rqworkers/djangoRqWorkers.sh
+++ rqworkers/djangoRqWorkers.sh
@@ -10,10 +10,9 @@ trap 'kill -TERM $(jobs -p); trap - INT;' TERM
 # Read config file and start workers
 while IFS='|' read -r -a line || [[ -n "$line" ]]; do
   for (( i=0; i<"${line[0]}"; i++ )) do
-    /usr/bin/python2.7 /home/server/Documents/server/manage.py \
-    rqworker ${line[1]} --worker-class "rqworkers.${line[2]}" &
+    <PYTHON_EXECUTABLE> <DJANGO_MANAGE_PY> rqworker ${line[1]} --worker-class "rqworkers.${line[2]}" &
     done
-done < /home/server/Documents/server/rqworkers/worker_config.txt
+done < <PATH_TO_WORKER_CONFIG_FILE>
 # First wait keeps the process running, waiting the workers to end
 wait
 # Second wait keeps the process running while the workers end gracefully
