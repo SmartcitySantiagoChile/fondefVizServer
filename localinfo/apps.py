5d1f89ef7887eeac71894af356152aa5b01068ed
Felipe Hernández <cephei.1313@gmail.com>
2018-03-06 14:44:06 -0300

    check there is not unapplied migrations

--- localinfo/apps.py
+++ localinfo/apps.py
@@ -3,12 +3,21 @@ from __future__ import unicode_literals
 
 from django.apps import AppConfig
 
+from io import BytesIO
+
 
 class LocalinfoConfig(AppConfig):
     name = 'localinfo'
 
     def ready(self):
         # create permissions if not exist
+        from django.core.management import call_command
         from localinfo.helper import PermissionBuilder
+
+        out = BytesIO()
+        call_command('showmigrations', '--list', 'localinfo', stdout=out)
+
+        if '[ ]' in out.getvalue():
+            return
 
         PermissionBuilder().create_permission_based_on_operators()
