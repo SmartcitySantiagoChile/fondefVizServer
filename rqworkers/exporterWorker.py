01657e2fefb184741ec2da62411a51dbe8c8b79a
Felipe Hernández <cephei.1313@gmail.com>
2018-03-06 17:39:18 -0300

    added exporter worker

---
+++ rqworkers/exporterWorker.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from rqworkers.killClass import KillWorker
+from rqworkers.dataUploader.loadData import upload_file
+
+
+class ExporterWorker(KillWorker):
+    """ Worker that preloads the necessary imports to upload data """
+
+    def __init__(self, queues, name=None, default_result_ttl=None, connection=None,
+                 exc_handler=None, exception_handlers=None, default_worker_ttl=None,
+                 job_class=None, queue_class=None):
+        super(ExporterWorker, self).__init__(queues, name=name, default_result_ttl=default_result_ttl,
+                                             connection=connection, exc_handler=exc_handler,
+                                             exception_handlers=exception_handlers,
+                                             default_worker_ttl=default_worker_ttl, job_class=job_class,
+                                             queue_class=queue_class)
