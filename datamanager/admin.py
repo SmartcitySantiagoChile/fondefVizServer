a7c29741dbf8e13cee6889d7eecf122e25014777
Felipe Hernández <cephei.1313@gmail.com>
2018-03-08 06:15:22 -0300

    added admin for job execution model

--- datamanager/admin.py
+++ datamanager/admin.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-from datamanager.models import DataSourcePath
+from datamanager.models import DataSourcePath, JobExecution
 
 
 class DataSourceAdmin(admin.ModelAdmin):
@@ -23,7 +23,29 @@ class DataSourceAdmin(admin.ModelAdmin):
         return False
 
     def get_readonly_fields(self, request, obj=None):
-        return ('timeStamp', 'code')
+        return 'timeStamp', 'code'
+
+
+class JobExecutionAdmin(admin.ModelAdmin):
+    """ manager for job execution """
+    fieldsets = (
+        (None, {'fields': ('jobId', 'type', 'status')}),
+        (None, {'fields': ('executionStart', 'executionEnd')}),
+        (None, {'fields': ('inputs', 'errorMessage')}),
+    )
+    list_filter = []
+    list_display = ('jobId', 'type', 'status', 'executionStart', 'executionEnd')
+    actions = None
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def get_readonly_fields(self, request, obj=None):
+        return 'jobId', 'type', 'status', 'executionStart', 'executionEnd', 'inputs', 'errorMessage'
 
 
 admin.site.register(DataSourcePath, DataSourceAdmin)
+admin.site.register(JobExecution, JobExecutionAdmin)
