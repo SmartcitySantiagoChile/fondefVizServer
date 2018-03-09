32112739daad24ae56840f1d8260b42a44613bdf
Felipe Hernández <cephei.1313@gmail.com>
2018-03-09 09:53:21 -0300

    fixed url pattern

--- datamanager/urls.py
+++ datamanager/urls.py
@@ -8,9 +8,9 @@ from datamanager.views import LoadManagerHTML, GetLoadFileData, LoadData, Delete
 
 app_name = 'datamanager'
 urlpatterns = [
-    url(r'^manager', staff_member_required(LoadManagerHTML.as_view()), name='loadmanager'),
-    url(r'^data', staff_member_required(GetLoadFileData.as_view()), name='getLoadFileData'),
-    url(r'^loadData', staff_member_required(LoadData.as_view()), name='loadData'),
-    url(r'^deleteData', staff_member_required(DeleteData.as_view()), name='deleteData'),
-    url(r'^cancelData', staff_member_required(CancelData.as_view()), name='cancelData')
+    url(r'^manager/$', staff_member_required(LoadManagerHTML.as_view()), name='loadmanager'),
+    url(r'^data/$', staff_member_required(GetLoadFileData.as_view()), name='getLoadFileData'),
+    url(r'^loadData/$', staff_member_required(LoadData.as_view()), name='loadData'),
+    url(r'^deleteData/$', staff_member_required(DeleteData.as_view()), name='deleteData'),
+    url(r'^cancelData/$', staff_member_required(CancelData.as_view()), name='cancelData')
 ]
