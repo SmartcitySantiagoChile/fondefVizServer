1b698d9aed63e268819834013f1b3307043d1f2a
Felipe Hernández <cephei.1313@gmail.com>
2018-03-11 22:30:05 -0300

    catch errors related to data upload process

--- rqworkers/tasks.py
+++ rqworkers/tasks.py
@@ -12,34 +12,39 @@ from rqworkers.dataUploader.loadData import upload_file
 from datamanager.models import UploaderJobExecution
 
 import time
+import sys
 
 
 @job('data_uploader')
 def upload_file_job(path_to_file):
+    job_instance = get_current_job()
     # wait until UploaderJobExecution instance exists
     while True:
         try:
-            job_execution_obj = UploaderJobExecution.objects.get(jobId=get_current_job().id)
+            job_execution_obj = UploaderJobExecution.objects.get(jobId=job_instance.id)
             break
         except UploaderJobExecution.DoesNotExist:
             time.sleep(1)
 
     job_execution_obj.executionStart = timezone.now()
     job_execution_obj.save()
-
-    upload_file(settings.ES_CLIENT, path_to_file)
+    try:
+        upload_file(settings.ES_CLIENT, path_to_file)
+    except Exception:
+        exc_type, exc_value, traceback = sys.exc_info()
+        exception_handler(job_instance, exc_type, exc_value, traceback)
 
     job_execution_obj.executionEnd = timezone.now()
     job_execution_obj.status = UploaderJobExecution.FINISHED
     job_execution_obj.save()
 
 
-def exception_handler(job, exc_type, exc_value, traceback):
-    job_execution_obj = UploaderJobExecution.objects.get(jobId=job.id)
+def exception_handler(job_instance, exc_type, exc_value, traceback):
+    job_execution_obj = UploaderJobExecution.objects.get(jobId=job_instance.id)
 
     job_execution_obj.executionEnd = timezone.now()
     job_execution_obj.status = UploaderJobExecution.FAILED
-    job_execution_obj.errorMessage = str(traceback)
+    job_execution_obj.errorMessage = '{0}\n{1}\n{2}'.format(exc_type, exc_value, traceback)
 
     # continue with the next handler if exists, for instance: failed queue
     return True
