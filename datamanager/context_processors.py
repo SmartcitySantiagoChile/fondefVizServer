88e035d8c5acc073ee40ab5658aa37ed469e934b
Felipe Hernández <cephei.1313@gmail.com>
2018-03-26 11:48:28 -0300

    return empty dict if user is anonymous

--- datamanager/context_processors.py
+++ datamanager/context_processors.py
@@ -7,10 +7,13 @@ from datamanager.models import ExporterJobExecution
 
 
 def export_job(request):
+    if request.user.is_anonymous:
+        return {}
+
     jobs = ExporterJobExecution.objects.filter(Q(seen=False) | Q(status=ExporterJobExecution.RUNNING),
                                                user=request.user)
     # if there is not running jobs or not seen jobs shows last 3
-    #if len(jobs) == 0:
+    # if len(jobs) == 0:
     #    jobs = ExporterJobExecution.objects.filter(user=request.user).order_by('-enqueueTimestamp')[:3]
 
     return {
