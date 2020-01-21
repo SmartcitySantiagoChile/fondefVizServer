9276a39a1b7b28d7a60dfb6de3e775b575599cef
Felipe Hernández <cephei.1313@gmail.com>
2020-01-21 15:32:22 -0300

    refactoring

--- esapi/helper/profile.py
+++ esapi/helper/profile.py
@@ -195,7 +195,7 @@ class ESProfileHelper(ElasticSearchHelper):
         combined_filter = []
         for date_range in dates:
             start_date = date_range[0]
-            end_date = date_range[len(date_range) - 1]
+            end_date = date_range[-1]
             if not start_date or not end_date:
                 raise ESQueryDateRangeParametersDoesNotExist()
             filter_q = Q("range", expeditionStartTime={
