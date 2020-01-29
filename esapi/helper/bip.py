539f70cb4a1a5dfad13edbfb12bade956eaac740
Bastianleaf <bastian.martinez@ing.uchile.cl>
2020-01-29 11:23:11 -0300

    Agregada información y refactoring.

--- esapi/helper/bip.py
+++ esapi/helper/bip.py
@@ -17,37 +17,8 @@ class ESBipHelper(ElasticSearchHelper):
         file_extensions = ['bip']
         super(ESBipHelper, self).__init__(index_name, file_extensions)
 
-
     def get_available_days(self):
         return self._get_available_days('validationTime')
-
-    def get_available_routes(self, valid_operator_list):
-        es_query = self.get_base_query()
-        es_query = es_query[:0]
-        es_query = es_query.source([])
-
-        if valid_operator_list:
-            es_query = es_query.filter('terms', operator=valid_operator_list)
-        else:
-            raise ESQueryOperatorParameterDoesNotExist()
-
-        aggs = A('terms', field="route", size=5000)
-        es_query.aggs.bucket('route', aggs)
-        es_query.aggs['route']. \
-            metric('additionalInfo', 'top_hits', size=1, _source=['operator', 'userRoute'])
-
-        operator_list = get_operator_list_for_select_input(filter=valid_operator_list)
-
-        result = defaultdict(lambda: defaultdict(list))
-        for hit in es_query.execute().aggregations.route.buckets:
-            data = hit.to_dict()
-            auth_route = data['key']
-            operator_id = data['additionalInfo']['hits']['hits'][0]['_source']['operator']
-            user_route = data['additionalInfo']['hits']['hits'][0]['_source']['userRoute']
-
-            result[operator_id][user_route].append(auth_route)
-
-        return result, operator_list
 
     def get_bip_by_operator_data(self, dates, valid_operator_list):
 
@@ -80,4 +51,3 @@ class ESBipHelper(ElasticSearchHelper):
             bucket('operators', 'terms', field='operator', size=1000)
 
         return es_query, operator_list
-
