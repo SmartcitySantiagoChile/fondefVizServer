49c6cef4502a8cbe68eec9422f9a046cc95cc6bd
Bastianleaf <bastian.martinez@ing.uchile.cl>
2020-01-31 12:36:27 -0300

    Refactoring

--- esapi/views/trip.py
+++ esapi/views/trip.py
@@ -414,7 +414,6 @@ class MultiRouteData(View):
             res.append({"item": hit.key})
         if len(res) == 0:
             raise ESQueryResultEmpty()
-        print(len(res))
         return {
             'data': res
         }
@@ -434,4 +433,3 @@ class MultiRouteData(View):
 
     def post(self, request):
         return self.process_request(request, request.POST, export_data=True)
-
