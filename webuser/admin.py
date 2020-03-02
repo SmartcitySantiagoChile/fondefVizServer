9f193f79b9fbee79ada593555b111fda8081cc49
Bastianleaf <bastian.martinez@ing.uchile.cl>
2020-03-02 11:22:00 -0300

    Se agrega último inicio de sesión a lista de usuarios.

--- webuser/admin.py
+++ webuser/admin.py
@@ -1,6 +1,13 @@
-# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from django.contrib.auth.models import User
 
-# Register your models here.
+admin.site.unregister(User)
+
+
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'last_login')
+
+
+admin.site.register(User, UserProfileAdmin)
