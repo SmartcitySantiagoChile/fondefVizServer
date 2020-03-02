f73021c197958ea1d2a9526277cf020c602139e8
Bastianleaf <bastian.martinez@ing.uchile.cl>
2020-03-02 11:36:33 -0300

    Refactoring

--- webuser/admin.py
+++ webuser/admin.py
@@ -2,12 +2,8 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
 
 admin.site.unregister(User)
-
-
-class UserProfileAdmin(admin.ModelAdmin):
-    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'last_login')
-
-
-admin.site.register(User, UserProfileAdmin)
+UserAdmin.list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'last_login')
+admin.site.register(User, UserAdmin)
