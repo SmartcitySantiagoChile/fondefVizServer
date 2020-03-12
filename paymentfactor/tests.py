2f18b3d89931a4a73fd06549e2b3be631793efba
Bastianleaf <bastian.martinez@ing.uchile.cl>
2020-03-12 13:01:39 -0300

    Test de paymentfactor view

--- paymentfactor/tests.py
+++ paymentfactor/tests.py
@@ -1,6 +1,14 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.test import TestCase
-
 # Create your tests here.
+from testhelper.helper import TestHelper
+
+
+class ConnectionTest(TestHelper):
+
+    def setUp(self):
+        self.client = self.create_logged_client_with_global_permission()
+
+    def test_site_operator(self):
+        self.check_http_response(self.client, 'paymentfactor:index', 200)
