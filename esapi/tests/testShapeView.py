7702f73b0c2cf1ee23d29b73f6277cd6bce66933
Bastianleaf <bastian.martinez@ing.uchile.cl>
2020-03-17 10:39:38 -0300

    Se trasladan los tests.

--- esapi/tests/testShapeView.py
+++ esapi/tests/testShapeView.py
@@ -5,6 +5,7 @@ import json
 
 import mock
 from django.urls import reverse
+from mock import patch
 
 from esapi.errors import ESQueryStopPatternTooShort
 from testhelper.helper import TestHelper
@@ -41,3 +42,38 @@ class MatchedStopDataTest(TestHelper):
         response = self.client.get(self.url, data)
         status = json.dumps(json.loads(response.content)['status'])
         self.assertJSONEqual(status, ESQueryStopPatternTooShort().get_status_response())
+
+
+class ConnectionTest(TestHelper):
+
+    def setUp(self):
+        self.client = self.create_logged_client_with_global_permission()
+
+    @patch('esapi.views.shape.ESShapeHelper')
+    @patch('esapi.views.shape.ESStopByRouteHelper')
+    def test_site_route(self, es_stop_by_route_helper, es_shape_helper):
+        es_shape_helper.return_value = es_shape_helper
+        es_shape_helper.get_route_shape.return_value = dict(points=[])
+        es_stop_by_route_helper.return_value = es_stop_by_route_helper
+        es_stop_by_route_helper.get_stop_list.return_value = dict(stops=[])
+        data = dict(route='abc', operationProgramDate='abc')
+        self.check_json_response(self.client, 'esapi:shapeRoute', 200, data)
+
+    def test_site_route_without_route_param(self):
+        data = dict(operationProgramDate='abc')
+        self.check_json_response(self.client, 'esapi:shapeRoute', 200, data)
+        # json_response = self.check_json_response(self.client, 'shape:route', 200, data)
+        # answer = json_response['status']
+        # print(answer)
+        # self.assertDictEqual(answer, ESQueryRouteParameterDoesNotExist().get_status_response())
+
+    def test_site_route_without_operation_program_date_param(self):
+        data = dict(route='abc')
+        self.check_json_response(self.client, 'esapi:shapeRoute', 200, data)
+        # json_response = self.check_json_response(self.client, 'shape:route', 200, data)
+        # answer = json_response['status']
+        # self.assertDictEqual(answer, ESQueryDateParametersDoesNotExist().get_status_response())
+
+    def test_site_base(self):
+        data = dict(route='', operationProgramDate='')
+        self.check_http_response(self.client, 'esapi:shapeBase', 200, data)
