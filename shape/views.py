6a39a95d8c40aa6715f0b49bfec4e2c3f82e0f68
Bastianleaf <bastian.martinez@ing.uchile.cl>
2020-03-17 10:10:16 -0300

    Se traslada lógica de shape/views a esapi/views/shape

--- shape/views.py
+++ shape/views.py
@@ -1,15 +1,13 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.views.generic import View
-from django.shortcuts import render
 from django.http import JsonResponse
+from django.shortcuts import render
+from django.views.generic import View
 
+from esapi.errors import FondefVizError, ESQueryRouteParameterDoesNotExist, ESQueryDateParametersDoesNotExist
 from esapi.helper.shape import ESShapeHelper
 from esapi.helper.stopbyroute import ESStopByRouteHelper
-from esapi.errors import FondefVizError, ESQueryRouteParameterDoesNotExist, ESQueryDateParametersDoesNotExist
-
-from datetime import datetime
 
 
 class GetRouteInfo(View):
@@ -34,33 +32,6 @@ class GetRouteInfo(View):
                 'stops']
         except FondefVizError as e:
             response['status'] = e.get_status_response()
-        return JsonResponse(response)
-
-
-class GetBaseInfo(View):
-
-    def get(self, request):
-        # retrieve route list and available days
-        es_shape_helper = ESShapeHelper()
-        es_stop_helper = ESStopByRouteHelper()
-
-        stop_dates = es_stop_helper.get_available_days()
-        shape_dates = es_shape_helper.get_available_days()
-
-        dates = list(set(stop_dates + shape_dates))
-        dates.sort(key=lambda x: datetime.strptime(x, '%Y-%m-%d'))
-
-        stop_routes = es_stop_helper.get_route_list()
-        shape_routes = es_shape_helper.get_route_list()
-
-        routes = list(set(stop_routes + shape_routes))
-        routes.sort()
-
-        response = {
-            'dates': dates,
-            'routes': routes
-        }
-
         return JsonResponse(response)
 
 
