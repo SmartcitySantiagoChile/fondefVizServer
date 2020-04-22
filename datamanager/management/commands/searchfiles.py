2c99e4a0ab63aa0b23f784abf6b8fd4a6f27dfcc
bastianleaf <bastian.martinez@ing.uchile.cl>
2020-04-22 15:47:11 -0400

    Se imprimen warnings para cuando los archivos ya existan en ES.

--- datamanager/management/commands/searchfiles.py
+++ datamanager/management/commands/searchfiles.py
@@ -35,8 +35,38 @@ class Command(BaseCommand):
             path_name = os.path.join(path, data_source_obj.filePattern)
             zipped_path_name = os.path.join(path, '{0}.zip'.format(data_source_obj.filePattern))
             gzipped_path_name = os.path.join(path, '{0}.gz'.format(data_source_obj.filePattern))
-            file_name_list = glob.glob(path_name) + glob.glob(zipped_path_name) + glob.glob(gzipped_path_name)
 
+            def clean_list(l, index):
+                return list(map(lambda x: x.split("/")[-1:][0], l))
+
+            path_name_list = glob.glob(path_name)
+            clean_name_list = clean_list(path_name_list, -2)
+            zipped_name_list = glob.glob(zipped_path_name)
+            clean_zipped_name_list = clean_list(zipped_name_list, -3)
+            gzipped_name_list = glob.glob(gzipped_path_name)
+            clean_gzipped_name_list = clean_list(gzipped_name_list, -3)
+            file_name_list = path_name_list + zipped_name_list + gzipped_name_list
+            files_name = list(map(lambda x: x.fileName, LoadFile.objects.all()))
+
+            for name in clean_name_list:
+                for file in files_name:
+                    if name in file:
+                        if name != file:
+                            print("WARNING:", name, "exist in elasticsearch as", file)
+
+            for name in clean_gzipped_name_list:
+                for file in files_name:
+                    name_without_gz = "".join(name.split("gz")[:-1])[:-1]
+                    if name_without_gz in file:
+                        if name != file:
+                            print("WARNING:", name, "exist in elasticsearch as", file)
+
+            for name in clean_zipped_name_list:
+                for file in files_name:
+                    name_without_zip = "".join(name.split("zip")[:-1])[:-1]
+                    if name_without_zip in file:
+                        if name != file:
+                            print("WARNING:", name, "exist in elasticsearch as", file)
             for file_path in file_name_list:
                 file_name = os.path.basename(file_path)
                 last_modified = timezone.make_aware(timezone.datetime.fromtimestamp(os.path.getmtime(file_path)))
