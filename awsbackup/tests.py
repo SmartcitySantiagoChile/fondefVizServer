e0e7dd33f33cff6ad0b3e316f871dbf26f661893
bastianleaf <bastian.martinez@ing.uchile.cl>
2020-05-08 17:45:45 -0400

    Refactoring.

--- awsbackup/tests.py
+++ awsbackup/tests.py
@@ -1,5 +1,8 @@
+from unittest import mock
+
 from django.apps import apps
 from django.test import TestCase
+from django.urls import reverse
 
 from awsbackup.apps import AwsbackupConfig
 from testhelper.helper import TestHelper
@@ -16,35 +19,68 @@ class ConnectionTest(TestHelper):
     def setUp(self):
         self.client = self.create_logged_client_with_global_permission()
 
-    def test_site_gps(self):
-        self.check_http_response(self.client, 'awsbackup:gps', 200)
+    @mock.patch('awsbackup.aws.AWSSession')
+    def test_site_gps(self, awsession):
+        url = reverse('awsbackup:gps')
+        response = self.client.get(url)
+        self.assertNotContains(response, 'status')
 
-    def test_site_trip(self):
-        self.check_http_response(self.client, 'awsbackup:trip', 200)
+    @mock.patch('awsbackup.aws.AWSSession')
+    def test_site_trip(self, awsession):
+        url = reverse('awsbackup:trip')
+        response = self.client.get(url)
+        self.assertNotContains(response, 'status')
 
-    def test_site_opprogram(self):
-        self.check_http_response(self.client, 'awsbackup:opprogram', 200)
+    @mock.patch('awsbackup.aws.AWSSession')
+    def test_site_opprogram(self, awsession):
+        url = reverse('awsbackup:opprogram')
+        response = self.client.get(url)
+        self.assertNotContains(response, 'status')
 
-    def test_site_representativeweek(self):
-        self.check_http_response(self.client, 'awsbackup:representativeweek', 200)
+    @mock.patch('awsbackup.aws.AWSSession')
+    def test_site_representativeweek(self, awsession):
+        url = reverse('awsbackup:representativeweek')
+        response = self.client.get(url)
+        self.assertNotContains(response, 'status')
 
-    def test_site_196(self):
-        self.check_http_response(self.client, 'awsbackup:196', 200)
+    @mock.patch('awsbackup.aws.AWSSession')
+    def test_site_196(self, awsession):
+        url = reverse('awsbackup:196')
+        response = self.client.get(url)
+        self.assertNotContains(response, 'status')
 
-    def test_site_profile(self):
-        self.check_http_response(self.client, 'awsbackup:profile', 200)
+    @mock.patch('awsbackup.aws.AWSSession')
+    def test_site_profile(self, awsession):
+        url = reverse('awsbackup:profile')
+        response = self.client.get(url)
+        self.assertNotContains(response, 'status')
 
-    def test_site_stage(self):
-        self.check_http_response(self.client, 'awsbackup:stage', 200)
+    @mock.patch('awsbackup.aws.AWSSession')
+    def test_site_stage(self, awsession):
+        url = reverse('awsbackup:stage')
+        response = self.client.get(url)
+        self.assertNotContains(response, 'status')
 
-    def test_site_transaction(self):
-        self.check_http_response(self.client, 'awsbackup:transaction', 200)
+    @mock.patch('awsbackup.aws.AWSSession')
+    def test_site_transaction(self, awsession):
+        url = reverse('awsbackup:transaction')
+        response = self.client.get(url)
+        self.assertNotContains(response, 'status')
 
-    def test_site_opspeed(self):
-        self.check_http_response(self.client, 'awsbackup:opspeed', 200)
+    @mock.patch('awsbackup.aws.AWSSession')
+    def test_site_opspeed(self, awsession):
+        url = reverse('awsbackup:opspeed')
+        response = self.client.get(url)
+        self.assertNotContains(response, 'status')
 
-    def test_site_stoptime(self):
-        self.check_http_response(self.client, 'awsbackup:stoptime', 200)
+    @mock.patch('awsbackup.aws.AWSSession')
+    def test_site_stoptime(self, awessesion):
+        url = reverse('awsbackup:stoptime')
+        response = self.client.get(url)
+        self.assertNotContains(response, 'status')
 
-    def test_site_activeDownloadLink(self):
-        self.check_http_response(self.client, 'awsbackup:activeDownloadLink', 200)
+    @mock.patch('awsbackup.aws.AWSSession')
+    def test_site_activeDownloadLink(self, awsession):
+        url = reverse('awsbackup:activeDownloadLink')
+        response = self.client.get(url)
+        self.assertNotContains(response, 'status')
