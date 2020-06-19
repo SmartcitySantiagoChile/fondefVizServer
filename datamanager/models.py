85b646dba5220bae02dfa97036a24f244182f6af
bastianleaf <bastian.martinez@ing.uchile.cl>
2020-06-19 19:17:00 -0400

    Se agrega opdata a fuenta de datos

--- datamanager/models.py
+++ datamanager/models.py
@@ -152,13 +152,15 @@ class ExporterJobExecution(JobExecution):
     PROFILE = 'profile'
     PAYMENT_FACTOR = 'paymentfactor'
     BIP = 'bip'
+    OPDATA = 'opdata'
     FILE_TYPE_CHOICES = (
         (OD_BY_ROUTE, 'Matriz de etapa por servicio'),
         (SPEED, 'Velocidades'),
         (TRIP, 'Viajes'),
         (PROFILE, 'Perfil de carga'),
         (PAYMENT_FACTOR, 'Validaciones en zona paga'),
-        (BIP, "Validaciones bip")
+        (BIP, "Validaciones bip"),
+        (OPDATA, "Programas de Operación")
     )
     fileType = models.CharField('Fuente de datos', max_length=30, null=False, choices=FILE_TYPE_CHOICES)
     # to save data downloader (name, filters, etc)
--- esapi/helper/opdata.py
+++ esapi/helper/opdata.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
-
-
+from esapi.errors import ESQueryDateRangeParametersDoesNotExist
 from esapi.helper.basehelper import ElasticSearchHelper
 
 
@@ -11,3 +10,8 @@ class ESOpDataHelper(ElasticSearchHelper):
         file_extensions = ['opdata']
         super(ESOpDataHelper, self).__init__(index_name, file_extensions)
 
+    def get_route_info(self, op_route, dates):
+        if not dates or not isinstance(dates[0], list) or not dates[0]:
+            raise ESQueryDateRangeParametersDoesNotExist()
+
+        es_query = self.get_base_query()
