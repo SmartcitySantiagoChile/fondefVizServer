b9679b902923de834191490946a3de5ac1d7ea6d
bastianleaf <bastian.martinez@ing.uchile.cl>
2020-06-19 15:18:32 -0400

    Correciones a tests

--- localinfo/tests/testViews.py
+++ localinfo/tests/testViews.py
@@ -6,6 +6,7 @@ from django.test import TestCase
 
 from localinfo.models import OPDictionary, CustomRoute
 from localinfo.tests.helper import TestHelper
+from django.urls import reverse
 
 
 class LocalInfoViewTest(TestCase):
@@ -17,24 +18,24 @@ class LocalInfoViewTest(TestCase):
 
     def test_OPDictionaryCsvUploader_post(self):
         with open(os.path.join(self.path, 'opdictionary.csv'), 'r') as file:
-            response = self.client.post('/csvuploadOP/', {'name': 'file.csv', 'csvDictionary': file})
+            response = self.client.post(reverse('opdictionarycsvupload'), {'name': 'file.csv', 'csvDictionary': file})
             saved_objects = OPDictionary.objects.all()
             self.assertEqual(200, response.status_code)
         with open(os.path.join(self.path, 'opdictionary.csv'), 'r') as file:
             csv_reader = csv.reader(file, delimiter=',')
             for row in csv_reader:
                 self.assertTrue(saved_objects.filter(auth_route_code=row[0], op_route_code=row[1]).exists())
-        bad_response = self.client.post('/csvuploadOP/', {'name': 'file.csv', 'csvDictionary': ''})
+        bad_response = self.client.post(reverse('opdictionarycsvupload'), {'name': 'file.csv', 'csvDictionary': ''})
         self.assertEqual(400, bad_response.status_code)
 
     def test_CustomRouteCsvUploader_post(self):
         with open(os.path.join(self.path, 'opdictionary.csv'), 'r') as file:
-            response = self.client.post('/csvupload/', {'name': 'file.csv', 'csvDictionary': file})
+            response = self.client.post(reverse('customroutecsvupload'), {'name': 'file.csv', 'csvDictionary': file})
             saved_objects = CustomRoute.objects.all()
             self.assertEqual(200, response.status_code)
         with open(os.path.join(self.path, 'opdictionary.csv'), 'r') as file:
             csv_reader = csv.reader(file, delimiter=',')
             for row in csv_reader:
                 self.assertTrue(saved_objects.filter(auth_route_code=row[0], custom_route_code=row[1]).exists())
-        bad_response = self.client.post('/csvupload/', {'name': 'file.csv', 'csvDictionary': ''})
+        bad_response = self.client.post(reverse('customroutecsvupload'), {'name': 'file.csv', 'csvDictionary': ''})
         self.assertEqual(400, bad_response.status_code)
