bc227bb95f67685f9e210e45a14ffac65ab3885d
bastianleaf <bastian.martinez@ing.uchile.cl>
2020-07-14 12:34:11 -0400

    Se modifican staticfiles para usar manifest.

--- fondefVizServer/settings.py
+++ fondefVizServer/settings.py
@@ -212,6 +212,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_STORAGE = 'django_forgiving_collectstatic.storages.ForgivingManifestStaticFilesStorage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
