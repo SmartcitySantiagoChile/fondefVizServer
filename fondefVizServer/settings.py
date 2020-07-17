e4945ff7569a9e175c136345bbc058d241b129f5
bastianleaf <bastian.martinez@ing.uchile.cl>
2020-07-17 11:05:22 -0400

    pruebas setting

--- fondefVizServer/settings.py
+++ fondefVizServer/settings.py
@@ -212,7 +212,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_STORAGE = 'django_forgiving_collectstatic.storages.ForgivingManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'storage.DjsManifestStaticFilesStorage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
