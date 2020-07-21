d1cf289a6b128c06e301649bed26ff76e56d9cc6
bastianleaf <bastian.martinez@ing.uchile.cl>
2020-07-21 10:29:43 -0400

    Se modifica ruta de storage.py

--- fondefVizServer/settings.py
+++ fondefVizServer/settings.py
@@ -212,7 +212,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_STORAGE = 'storage.CustomManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'fondefVizServer.storage.CustomManifestStaticFilesStorage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
