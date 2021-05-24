fe0439be6c53d33704bca5bec36af3c1b6253fb5
Bastianleaf <bastian.martinez@ing.uchile.cl>
2021-05-24 12:47:45 -0400

    Update get all time periods from odbyroute

--- esapi/helper/odbyroute.py
+++ esapi/helper/odbyroute.py
@@ -156,5 +156,5 @@ class ESODByRouteHelper(ElasticSearchHelper):
         es_query = self.get_base_query()
         es_query = es_query[:0]
         es_query.aggs.bucket('time_periods_per_file', 'terms', field='path', size=5000) \
-            .bucket('time_periods', 'terms', field='timePeriodInStopTime')
+            .bucket('time_periods_0', 'terms', field='timePeriodInStopTime')
         return es_query
