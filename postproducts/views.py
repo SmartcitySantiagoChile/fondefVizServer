e769120b996ef9e8346852929efe2c8badbb6706
Bastianleaf <bastian.martinez@ing.uchile.cl>
2021-08-17 17:54:32 -0400

    Add new permission to postproducts

--- postproducts/views.py
+++ postproducts/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.views.generic import View
+from django.contrib.auth.mixins import PermissionRequiredMixin
 
 from localinfo.helper import get_day_type_list_for_select_input, get_commune_list_for_select_input
 
 
-class TransfersHTML(View):
+class TransfersHTML(PermissionRequiredMixin, View):
+    permission_required = 'localinfo.postproducts'
 
     def get(self, request):
         template = "postproducts/transfers.html"
@@ -19,7 +21,8 @@ class TransfersHTML(View):
         return render(request, template, context)
 
 
-class TripsBetweenZonesHTML(View):
+class TripsBetweenZonesHTML(PermissionRequiredMixin, View):
+    permission_required = 'localinfo.postproducts'
 
     def get(self, request):
         template = "postproducts/tripsBetweenZones.html"
@@ -34,7 +37,8 @@ class TripsBetweenZonesHTML(View):
         return render(request, template, context)
 
 
-class BoardingAndAlightingByZoneHTML(View):
+class BoardingAndAlightingByZoneHTML(PermissionRequiredMixin, View):
+    permission_required = 'localinfo.postproducts'
 
     def get(self, request):
         template = "postproducts/boardingAndAlightingByZone.html"
