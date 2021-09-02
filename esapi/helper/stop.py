24d0745bd4bff0dd0e9e4b31dde3f1326c4c6f8d
Bastianleaf <bastian.martinez@ing.uchile.cl>
2021-09-02 18:49:30 -0400

    Add date to stop match query

--- esapi/helper/stop.py
+++ esapi/helper/stop.py
@@ -13,20 +13,63 @@ class ESStopHelper(ElasticSearchHelper):
         file_extensions = ['stop']
         super(ESStopHelper, self).__init__(index_name, file_extensions)
 
-    def get_matched_stop_list(self, term):
-        """ ask to elasticsearch for a match values """
+    def get_matched_stop_list(self, term, date=None):
+        """ Ask to elasticsearch for a stop match values based on date.
+        Args:
+           term: stop term
+           date: op program date
 
-        es_auth_stop_query = Search().query(Match(authCode={"query": term, "analyzer": "standard"}))[:0]
+        Returns: matched stop
+
+        """
+        es_auth_stop_query = Search()
+        es_user_stop_query = Search()
+        es_user_stop_name_query = Search()
+        if date:
+            es_auth_stop_query = es_auth_stop_query.query("match", startDate=date)
+            es_user_stop_query = es_user_stop_query
+            es_user_stop_name_query = es_user_stop_name_query
+
+        es_auth_stop_query = es_auth_stop_query.query(Match(authCode={"query": term, "analyzer": "standard"}))[:0]
         aggs = A('terms', field='authCode.raw', size=100)
         es_auth_stop_query.aggs.bucket('unique', aggs)
         es_auth_stop_query.aggs['unique'].metric('additional_info', 'top_hits', size=1, _source=['authCode'])
 
-        es_user_stop_query = Search().query(Match(userCode={"query": term, "analyzer": "standard"}))[:0]
+        es_user_stop_query = es_user_stop_query.query(Match(userCode={"query": term, "analyzer": "standard"}))[:0]
         aggs = A('terms', field='userCode.raw', size=100)
         es_user_stop_query.aggs.bucket('unique', aggs)
         es_user_stop_query.aggs['unique'].metric('additional_info', 'top_hits', size=1, _source=['authCode'])
 
-        es_user_stop_name_query = Search().query(
+        es_user_stop_name_query = es_user_stop_name_query.query(
+            Match(name={"query": term, "operator": "and", "analyzer": "autocomplete_analyzer"}))[:0]
+        aggs = A('terms', field='name.raw', size=100, order={"max_score": "desc"})
+        es_user_stop_name_query.aggs.bucket('unique', aggs)
+        es_user_stop_name_query.aggs['unique'].metric('max_score', 'max', script={'source': '_score'})
+        es_user_stop_name_query.aggs['unique'].metric('additional_info', 'top_hits', _source=['authCode'])
+
+        results = self.make_multisearch_query_for_aggs(es_auth_stop_query, es_user_stop_query, es_user_stop_name_query)
+
+        answer = []
+        for result in results:
+            answer += [(ans.key, ans.additional_info.hits[0].authCode) for ans in result.aggregations.unique.buckets]
+
+        return answer
+
+    def get_matched_stop_list_by_date(self, term, date):
+
+        es_auth_stop_query = Search().query("match", startDate=date).query(
+            Match(authCode={"query": term, "analyzer": "standard"}))[:0]
+        aggs = A('terms', field='authCode.raw', size=100)
+        es_auth_stop_query.aggs.bucket('unique', aggs)
+        es_auth_stop_query.aggs['unique'].metric('additional_info', 'top_hits', size=1, _source=['authCode'])
+
+        es_user_stop_query = Search().query("match", startDate=date).query(
+            Match(userCode={"query": term, "analyzer": "standard"}))[:0]
+        aggs = A('terms', field='userCode.raw', size=100)
+        es_user_stop_query.aggs.bucket('unique', aggs)
+        es_user_stop_query.aggs['unique'].metric('additional_info', 'top_hits', size=1, _source=['authCode'])
+
+        es_user_stop_name_query = Search().query("match", startDate=date).query(
             Match(name={"query": term, "operator": "and", "analyzer": "autocomplete_analyzer"}))[:0]
         aggs = A('terms', field='name.raw', size=100, order={"max_score": "desc"})
         es_user_stop_name_query.aggs.bucket('unique', aggs)
